c727a3c4486ff883d692d5bbf7ed49bbc858cc91
shexa-martin <martinmasheka@gmail.com>
2022-07-20 15:09:47 +0200

    Method not found error

---
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package net.javaguides.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+
+import net.javaguides.springboot.service.EmployeeService;
+
+@Controller
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeService employeeService;
+	
+	//a method handler for Home page (index.html) to display a list of employees
+	@GetMapping("/")
+	public String viewHomePage(Model model) {
+		//Spring boot auto configure view resolver for theme leaf
+		model.addAttribute("listEmployees", employeeService.getAllEmployees());
+		return "index";
+	}
+
+}
